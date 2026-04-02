8bdbb17a9be99d1716d2477c07c7dcd39644916b
RoiMoldus <yoann.michel.chupin@gmail.com>
2026-04-02 09:00:51 +0200

    PartieV0.1

---
+++ src/Partie.java
@@ -0,0 +1,88 @@
+public class Partie {
+    private int ressources;
+    private float production_energie;
+    private int nb_tour;
+    private int limite_tour;
+    private int nb_actions;
+    private String difficulter;
+    private boolean limite_depassee;
+
+    public Partie(String difficulter, boolean limite_depassee, int limite_tour, int nb_actions, int nb_tour, float production_energie, int ressources) {
+        this.difficulter = difficulter;
+        this.limite_depassee = limite_depassee;
+        this.limite_tour = limite_tour;
+        this.nb_actions = nb_actions;
+        this.nb_tour = nb_tour;
+        this.production_energie = production_energie;
+        this.ressources = ressources;
+    }
+
+    public int getRessources() {
+        return ressources;
+    }
+
+    public float getProduction_energie() {
+        return production_energie;
+    }
+
+    public int getNb_tour() {
+        return nb_tour;
+    }
+
+    public int getLimite_tour() {
+        return limite_tour;
+    }
+
+    public int getNb_actions() {
+        return nb_actions;
+    }
+
+    public String getDifficulter() {
+        return difficulter;
+    }
+
+    public boolean isLimite_depassee() {
+        return limite_depassee;
+    }
+
+    public void setRessources(int ressources) {
+        this.ressources = ressources;
+    }
+
+    public void setProduction_energie(float production_energie) {
+        this.production_energie = production_energie;
+    }
+
+    public void setNb_tour(int nb_tour) {
+        this.nb_tour = nb_tour;
+    }
+
+    public void setLimite_tour(int limite_tour) {
+        this.limite_tour = limite_tour;
+    }
+
+    public void setNb_actions(int nb_actions) {
+        this.nb_actions = nb_actions;
+    }
+
+    public void setDifficulter(String difficulter) {
+        this.difficulter = difficulter;
+    }
+
+    public void setLimite_depassee(boolean limite_depassee) {
+        this.limite_depassee = limite_depassee;
+    }
+
+    public void Fin_Tour() {
+        System.out.println("partie Fin_Tour");
+        this.Show();
+    }
+
+    public void Show() {
+        System.out.println("partie show");
+    }
+
+    
+    
+
+}
