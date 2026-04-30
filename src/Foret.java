f94bbbacafa9ec6bd68ef1774ca1dbfcf99e53a3
TitouanRd <titouan.regeard@gmail.com>
2026-04-30 10:02:54 +0200

    rajoue de la info case2
    rajoue meteo et mise au point de la taille de la fenetre

--- src/Foret.java
+++ src/Foret.java
@@ -41,12 +41,14 @@ public class Foret extends Case {
         infoLabel.setAlignmentX(panel.CENTER_ALIGNMENT);
         panel.add(infoLabel);
 
-        JTextArea infoArea = new JTextArea(5, 20);
+        JTextArea infoArea = new JTextArea(10, 20);
         infoArea.setEditable(false);
         infoArea.setText(
             "- Pollution: " + getPollution() + "\n" +
             "- Qualité: " + getQualite() + "\n" +
             "- Santé environnementale: " + getSante_environnemental() + "\n" +
+            "- Meteo: " + getMeteo() + "\n" +
+            "- Construction: " + getConstruction() + "\n" +
             "- Recouvrement d'arbres: " + recouvrementArbre
         );
         panel.add(new JScrollPane(infoArea));
